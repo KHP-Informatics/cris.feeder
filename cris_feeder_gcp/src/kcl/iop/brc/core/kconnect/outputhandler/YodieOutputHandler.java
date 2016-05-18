b468dc1962d4cae78933839102502f2251b3de07
honghan <honghan.wu@gmail.com>
2016-05-18 13:21:01 +0100

    - added the output handler

---
+++ cris_feeder_gcp/src/kcl/iop/brc/core/kconnect/outputhandler/YodieOutputHandler.java
@@ -0,0 +1,121 @@
+package kcl.iop.brc.core.kconnect.outputhandler;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import org.apache.commons.io.FileUtils;
+
+import kcl.iop.brc.core.kconnect.utils.JSONUtils;
+import ws.nuist.util.Configurator;
+import gate.Annotation;
+import gate.AnnotationSet;
+import gate.Document;
+import gate.cloud.batch.AnnotationSetDefinition;
+import gate.cloud.batch.DocumentID;
+import gate.cloud.io.OutputHandler;
+import gate.util.GateException;
+
+public class YodieOutputHandler implements OutputHandler{
+	OutputSetting[] anns;
+	String outputFilePrefix;
+	List<AnnotationSetDefinition> annotationDefs = null;
+
+	@Override
+	public void close() throws IOException, GateException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void config(Map<String, String> arg0) throws IOException,
+			GateException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<AnnotationSetDefinition> getAnnSetDefinitions() {
+		return annotationDefs;
+	}
+
+	@Override
+	public void init() throws IOException, GateException {
+		anns = 
+				JSONUtils.fromJSON( Configurator.getConfig("annotationOutputSettings"), OutputSetting[].class );
+		outputFilePrefix = Configurator.getConfig("outputFilePrefix");
+	}
+
+	@Override
+	public void outputDocument(Document doc, DocumentID docId)
+			throws IOException, GateException {
+		if (anns != null){
+			List<AnnotationSet> outputAnns = new LinkedList<AnnotationSet>();
+			for(OutputSetting os : anns){
+				AnnotationSet as = doc.getAnnotations(os.getAnnotationSet());
+				for (String type : os.getAnnotationType()){
+					outputAnns.add(as.get(type));
+//					Iterator<Annotation> ait = as.get(type).iterator();
+//					while(ait.hasNext()){
+//						Annotation an = ait.next();
+//						for(Object key : an.getFeatures().keySet()){
+//							an.getFeatures().get(key);
+//						}
+//					}
+				}
+			}
+			OutputData od = new OutputData();
+			od.setDocId(doc.getFeatures().get("id").toString());
+			od.setAnnotations(outputAnns);
+			String s = JSONUtils.toJSON(od) + "\n";
+			
+			File f = new File(outputFilePrefix + Thread.currentThread().getId());
+			FileUtils.writeStringToFile(f, s, true);
+		}
+	}
+
+	@Override
+	public void setAnnSetDefinitions(List<AnnotationSetDefinition> arg0) {
+		this.annotationDefs = arg0;
+	}
+
+	static class OutputSetting{
+		String annotationSet;
+		String[] annotationType;
+		public String getAnnotationSet() {
+			return annotationSet;
+		}
+		public void setAnnotationSet(String annotationSet) {
+			this.annotationSet = annotationSet;
+		}
+		public String[] getAnnotationType() {
+			return annotationType;
+		}
+		public void setAnnotationType(String[] annotatinoType) {
+			this.annotationType = annotatinoType;
+		}
+		
+	}
+	
+	static class OutputData{
+		String docId;
+		List<AnnotationSet> annotations;
+		public String getDocId() {
+			return docId;
+		}
+		public void setDocId(String docId) {
+			this.docId = docId;
+		}
+		public List<AnnotationSet> getAnnotations() {
+			return annotations;
+		}
+		public void setAnnotations(List<AnnotationSet> annotations) {
+			this.annotations = annotations;
+		}
+		
+	}
+}
---
+++ cris_feeder_gcp/src/kcl/iop/brc/core/kconnect/utils/JSONUtils.java
@@ -0,0 +1,83 @@
+
+/*
+ * $Id: JSONUtils.java,v 1.1 2012/02/01 00:00:46 hhwu Exp $
+ */
+
+package kcl.iop.brc.core.kconnect.utils;
+
+import java.io.StringWriter;
+
+import org.codehaus.jackson.map.ObjectMapper;
+import org.codehaus.jackson.type.TypeReference;
+
+
+/**
+ * Some basic utilities for processing JSON.  This class depends on Jackson.
+ *
+ * @author Jeff Schnitzer
+ */
+public class JSONUtils
+{
+        /**
+         * Converts the object to a JSON string
+         */
+        public static String toJSON(Object value)
+        {
+                ObjectMapper mapper = new ObjectMapper();
+                try
+                {
+                        StringWriter writer = new StringWriter();
+
+                        mapper.writeValue(writer, value);
+
+                        return writer.toString();
+
+                        // This requires Jackson 1.3 which breaks GAE.
+                        // TODO, replace with this code when bug is fixed:
+                        // http://jira.codehaus.org/browse/JACKSON-188
+                        //return mapper.writeValueAsString(value);
+                }
+                catch (RuntimeException ex) { throw ex; }
+                catch (Exception ex) { throw new RuntimeException(ex); }
+        }
+
+        /**
+         * Converts the JSON string to an Object (either Map or List)
+         */
+        public static Object fromJSON(String value)
+        {
+                return fromJSON(value, Object.class);
+        }
+
+        /**
+         * Converts the JSON string to a typed object via a TypeReference
+         * The main complication is handling of Generic types: if they are used, one
+         * has to use TypeReference object, to work around Java Type Erasure.
+         *
+         * ex: return JSONUtils.fromJSON(this.answersJson, new TypeReference<List<StanzaAnswer>>(){});
+         */
+        public static <T> T fromJSON(String value, TypeReference<T> type)
+        {
+                ObjectMapper mapper = new ObjectMapper();
+                try
+                {
+                        return mapper.readValue(value, type);
+                }
+                catch (RuntimeException ex) { throw ex; }
+                catch (Exception ex) { throw new RuntimeException(ex); }
+        }
+
+        /**
+         * Converts the JSON string to a typed object (or Map/List if Object.class is passed in)
+         */
+        public static <T> T fromJSON(String value, Class<T> type)
+        {
+                ObjectMapper mapper = new ObjectMapper();
+                try
+                {
+                        return mapper.readValue(value, type);
+                }
+                catch (RuntimeException ex) { throw ex; }
+                catch (Exception ex) { throw new RuntimeException(ex); }
+        }
+}
